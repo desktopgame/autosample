497893eea2ca5e2319cf5075bbfcff2bca59f056
Koya <koya0306@outlook.jp>
2021-01-19 08:39:47 +0900

    フラグを正しく判別できていなかったのを修正

--- audio_common.cpp
+++ audio_common.cpp
@@ -4,25 +4,21 @@ RtAudioFormat select_format(RtAudioFormat formats, RtAudioFormat& oneFormat,
                             int& formatN, std::string& name) {
   RtAudioFormat ret = 0;
   name = "signed-integer";
-  if (formats | RTAUDIO_SINT32) {
+  if ((formats & RTAUDIO_SINT32) > 0) {
     ret = RTAUDIO_SINT32;
     formatN = 32;
-  } else if (formats | RTAUDIO_SINT24) {
+  } else if ((formats & RTAUDIO_SINT24) > 0) {
     ret = RTAUDIO_SINT24;
     formatN = 24;
-  } else if (formats | RTAUDIO_SINT16) {
+  } else if ((formats & RTAUDIO_SINT16) > 0) {
     ret = RTAUDIO_SINT16;
     formatN = 16;
-  } else if (formats | RTAUDIO_SINT8) {
+  } else if ((formats & RTAUDIO_SINT8) > 0) {
     ret = RTAUDIO_SINT8;
     formatN = 8;
-  } else if (formats | RTAUDIO_FLOAT32) {
+  } else if ((formats & RTAUDIO_FLOAT32) > 0) {
     ret = RTAUDIO_FLOAT32;
     formatN = 32;
-    name = "floating-point";
-  } else if (formats | RTAUDIO_FLOAT64) {
-    ret = RTAUDIO_FLOAT64;
-    formatN = 64;
     name = "floating-point";
   }
   oneFormat = ret;
